a90eb788e6c4b9904a314d9c022b938dae8bccfe
Rahnken <ericdonnelly4@gmail.com>
2023-11-19 14:35:04 -0500

    initial starter code

---
+++ app.ts
@@ -0,0 +1,49 @@
+// External Modules
+import createError, { HttpError } from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import { Request, Response, NextFunction } from "express";
+
+// Routers
+import indexRouter from "./routes/index";
+import apiRouter from "./routes/api";
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules")));
+
+app.use("/", indexRouter);
+app.use("/api", apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+export default app;
